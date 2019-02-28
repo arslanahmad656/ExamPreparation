3e3bfe349165bd938cfe1ab854370a31672f69a4
Arslan Ahmad <ars.ahm6@gmail.com>
2019-02-28 18:09:06 +0500

    A new project added. AlarmClock class added.

---
+++ ExamPreparation/ClassLibrary1/AlarmClock/AlarmClock.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.AlarmClock
+{
+    class AlarmClock
+    {
+        private static int _interval;
+        private object _lock = new object();
+        private CancellationTokenSource _cancellationToken = new CancellationTokenSource();
+        private Task _clockRunner;
+
+        public Action OnAlarmRaised;
+
+        public int Interval
+        {
+            get => _interval;
+            set => _interval = value;
+        }
+
+        public AlarmClock(int intervalMilliseconds)
+        {
+            _interval = intervalMilliseconds;
+        }
+
+        public bool StartClock()
+        {
+            if (_clockRunner == null)
+            {
+                _clockRunner = new Task(() =>
+                {
+                    OnAlarmRaised?.Invoke();
+                    Thread.Sleep(Interval);
+                }, _cancellationToken.Token);
+            }
+
+            if (_clockRunner.Status == TaskStatus.Running)
+            {
+                return false;
+            }
+
+            _clockRunner.Start();
+            return true;
+        }
+
+        public bool StopClock()
+        {
+            if (_clockRunner != null && _clockRunner.Status == TaskStatus.Running)
+            {
+                _cancellationToken.Cancel();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ExamPreparation/Project2/Events/Events1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2.Events
+{
+    class Events1
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}

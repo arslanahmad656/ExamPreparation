be399f5974674eff2c35cd13bcda8e841fe2f874
Arslan Ahmad <ars.ahm6@gmail.com>
2019-03-15 16:46:35 +0500

    dsfdsfdsf

---
+++ ExamPreparation/Project6/COM/Example1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project6.COM
+{
+    static class Example1
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}

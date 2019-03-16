ba72230ff6639b530878d455dce66cf97a8ba160
Arslan Ahmad 656 <Arslan Ahmad 656@RAVIAN656--PC>
2019-03-16 21:59:09 +0500

    Project for COM added

---
+++ ExamPreparation/COMComponent/ISampleCOM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace COMComponent
+{
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    [Guid("c444c41e-4bb7-4ec6-a5ef-5ae069a28c21")]
+    public interface ISampleCOM
+    {
+        [DispId(1)]
+        int GetInteger(int input);
+    }
+}
---
+++ ExamPreparation/COMComponent/SampleCOM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace COMComponent
+{
+    [ClassInterface(ClassInterfaceType.None)]
+    [Guid("ff291463-a426-4af8-8565-f28a76dea520")]
+    public class SampleCOM : ISampleCOM
+    {
+        public int GetInteger(int input)
+        {
+            return input * 2;
+        }
+    }
+}

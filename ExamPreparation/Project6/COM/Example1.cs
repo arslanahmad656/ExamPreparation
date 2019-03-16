a2a99a4427313743853fc171a375f75bf6cd681c
Arslan Ahmad 656 <Arslan Ahmad 656@RAVIAN656--PC>
2019-03-16 12:15:53 +0500

    Excel interop example 2 added

--- ExamPreparation/Project6/COM/Example1.cs
+++ ExamPreparation/Project6/COM/Example1.cs
@@ -11,7 +11,8 @@ namespace Project6.COM
     {
         public static void Run()
         {
-            Demo1();
+            //Demo1();
+            Demo2();
         }
 
         static void Demo1()
@@ -21,10 +22,30 @@ namespace Project6.COM
                 Visible = true
             };
 
-            var workbook = excelApp.Workbooks.Add();
-            excelApp.Cells[1, 1].Font.FontStyle = "Bold";   // Cells[1, 1] has static type of Excel.Range but is used like dynamic
+            var workbook = excelApp.Workbooks.Add();    // interesting... the syntax of optional argument. It's an enhacement for COM
+            excelApp.Cells[1, 1].Font.FontStyle = "Bold";   // Cells[1, 1] has static type of Excel.Range but is used like dynamic. Another COM enhancement
             excelApp.Cells[1, 1].Value2 = "Hello Excel!";
             workbook.SaveAs(System.IO.Path.Combine(Environment.CurrentDirectory, "sheet.xlsx"));
+
+            // the above anomolities happen only for COM operations.
+        }
+
+        static void Demo2()
+        {
+            // the old fashioned way to interact with COM
+
+            var missingValue = System.Reflection.Missing.Value; // a value to represent a value that the COM method expects but is not supplied from .NET
+            var excelApp = new Excel.Application
+            {
+                Visible = true,
+            };
+
+            var workbook = excelApp.Workbooks.Add(missingValue); // without COM enhancement, the syntax in Demo1 will look like this one
+            var cells = (Excel.Range)excelApp.Cells[1, 1];  // Had there been not COM enhancements since C# 4.0, Cells[1, 1] could not be accessed like dynamic way. It had to be done like this one
+            cells.Font.FontStyle = "Bold";
+            cells.Value2 = "Hello from C# earlier than 4.0";
+            workbook.SaveAs(System.IO.Path.Combine(Environment.CurrentDirectory, "sheet2.xlsx"), missingValue, missingValue, missingValue,
+                missingValue, missingValue, Excel.XlSaveAsAccessMode.xlNoChange, missingValue, missingValue, missingValue, missingValue, missingValue);
         }
     }
 }

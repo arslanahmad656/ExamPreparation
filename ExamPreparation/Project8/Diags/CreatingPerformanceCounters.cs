6a64e2187384093429c1b21511cf65430c693c2e
Arslan Ahmad <ars.ahm6@gmail.com>
2019-04-15 17:26:08 +0500

    Sample Images added

---
+++ ExamPreparation/Project8/Diags/CreatingPerformanceCounters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Project8.Diags
+{
+    static class CreatingPerformanceCounters
+    {
+        static PerformanceCounter TotalImagesCounter;
+        static PerformanceCounter ImagesProcessPerSecondCounter;
+
+        public static void Run()
+        {
+            Demo();
+        }
+
+        static void Demo()
+        {
+
+        }
+
+        
+    }
+
+    enum CounterSetupResult
+    {
+        Created,
+        Loaded
+    }
+}

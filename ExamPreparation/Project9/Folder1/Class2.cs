3fa5ac31ad8182746b26e1fa9ae38792e61aaccc
Arslan Ahmad <ars.ahm6@gmail.com>
2019-04-18 19:21:04 +0500

    dsfdsfds

--- ExamPreparation/Project9/Folder1/Class2.cs
+++ ExamPreparation/Project9/Folder1/Class2.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Xml;
 using System.Runtime.Serialization;
+using System.Diagnostics;
 
 namespace Project9.Folder1
 {
@@ -13,7 +14,45 @@ namespace Project9.Folder1
     {
         public static void Run()
         {
-            WriteXmlDocument();
+            //WriteXmlDocument();
+            //DisposeCheck();
+            LogCheck();
+        }
+
+        static void LogCheck()
+        {
+            BaseLogger logger = new Logger();
+            logger.Log("Log started");
+            
+            ((Logger)logger).LogCompleted();
+        }
+
+        static void DisposeCheck()
+        {
+            Disposable disposable1;
+            using (disposable1 = new Disposable())
+            {
+                disposable1.OnDisposed += (sender, e) => DisposedEventHandler(nameof(disposable1));
+            }
+
+            using (var disposable2 = new Disposable())
+            {
+                disposable2.OnDisposed += (sender, e) => DisposedEventHandler(nameof(disposable2));
+            }
+
+            void DisposedEventHandler(string name)
+            {
+                Console.WriteLine($"{name} disposed");
+            }
+        }
+
+        static void CheckDirectives()
+        {
+#if DEBUG
+            Console.WriteLine("Entering debug mode");
+#else
+            Console.WriteLine("Entering release mode");
+#endif
         }
 
         static void WriteXmlDocument()
@@ -35,5 +74,47 @@ namespace Project9.Folder1
 
             writer.Flush();
         }
+    }
+
+    abstract class BaseLogger
+    {
+        public virtual void Log(string message)
+        {
+            Console.WriteLine("Base: " + message);
+        }
+
+        public void LogCompleted()
+        {
+            Console.WriteLine("Finished");
+        }
+    }
+
+    class Logger : BaseLogger
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public new void LogCompleted()
+        {
+            Console.WriteLine("Finished");
+        }
+    }
+
+    class Disposable : IDisposable
+    {
+        public event EventHandler<ObjectDisposedEventArgs> OnDisposed;
+
+        public void Dispose()
+        {
+            Console.WriteLine("Disposed");
+            OnDisposed?.Invoke(new object(), new ObjectDisposedEventArgs());
+        }
+    }
+
+    class ObjectDisposedEventArgs
+    {
+
     }
 }

732cf1f566748eb1011f2825efcac055a21bc7b4
Arslan Ahmad 656 <Arslan Ahmad 656@RAVIAN656--PC>
2019-06-08 12:20:54 +0500

    fazool projects removed

--- ExamPreparation/SignedAssemblyForTesting1/Properties/AssemblyInfo.cs
+++ ExamPreparation/SignedAssemblyForTesting1/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SignedAssemblyForTesting1")]
+[assembly: AssemblyProduct("TargetAssembly")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
